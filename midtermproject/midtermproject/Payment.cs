27bc8c4e35d59e45713e7160283d024b8f93fe99
Maya Araquil <mayaaraquil775@gmail.com>
2023-09-06 20:10:50 -0400

    NewThings

--- midtermproject/midtermproject/Payment.cs
+++ midtermproject/midtermproject/Payment.cs
@@ -8,5 +8,22 @@ namespace midtermproject
         public abstract void CompletePayment();
 
         public abstract void GatherPaymentDetails();
+        public decimal SalesTax { get; private set; }
+        public decimal SubTotal { get; private set; }
+        public decimal GrandTotal { get; private set; }
+        public PaymentType Type { get; set; }
+
+        public Payment(decimal salesTaxRate, PaymentType type)
+        {
+            SalesTax = salesTaxRate;
+            Type = type;
+        }
+
+        public void SetItemDetails(decimal price, int quantity)
+        {
+            SubTotal += price * quantity;
+        }
+
+        public void CalculateSalesTax
     }
 }
